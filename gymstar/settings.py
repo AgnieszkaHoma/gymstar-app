d280a31772dddb7b9a990af7a7088260fddbb45d
AgnieszkaHoma <homa.a@op.pl>
2023-02-16 23:45:44 +0100

    deployment

--- gymstar/settings.py
+++ gymstar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-%1+@+t68pb4nq87^vlmq*$p6jtu1&8t)&mww1g75c*26p8(zo^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
